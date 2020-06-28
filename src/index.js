182760fbad3be8014c32c5aa53f9a146cef52290
sAVItar02 <59026666+sAVItar02@users.noreply.github.com>
2020-06-28 15:39:07 +0530

    patch and delete routes added

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 
+//-------------------------------------------------------------------------------------------------------------------
 //----------------------------------------------------------USERS----------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------
 
 app.post('/users', async (req, res) => {
     const user = new User(req.body)
@@ -48,7 +50,50 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
+
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'age', 'email', 'password']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    
+    if(!isValidOperation)
+    {
+        return res.status(400).send({ error: 'Invalid Updates!' })
+    }
+    
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        
+        if(!user){
+            res.status(404).send()
+        }
+        else{
+            res.send(user)
+        }
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+app.delete('/users/:id', async (req, res) => {
+    
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if(!user) {
+            res.status(404).send()
+        }
+        else {
+            res.send(user)
+        }
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+//---------------------------------------------------------------------------------------------------------------------
 //-----------------------------------------------------TASKS-----------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------
 
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
@@ -87,7 +132,47 @@ app.get('/tasks/:id', async (req, res) => {
     }
 })
 
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every( (update) => allowedUpdates.includes(update) )
+
+    if(!isValidOperation){
+        return res.status(400).send({ error: 'Invalid Update!' })
+    }
+
+    try{
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if(!task){
+            res.status(404).send()
+        }
+        else {
+            res.send(task)
+        }
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if(!task) {
+            res.status(404).send()
+        } else {
+            res.send(task)            
+        }
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+//---------------------------------------------------------------------------------------------------------------------
 //-----------------------------------------------------LISTEN----------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------
 
 app.listen(PORT, () => {
     console.log("Server listening on port " + PORT )
