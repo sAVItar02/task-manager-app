5a6b746d9fc0a445c65fc34cd1cf06b947d28c5e
sAVItar02 <59026666+sAVItar02@users.noreply.github.com>
2020-07-04 16:26:02 +0530

    serving images

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const router = new express.Router()
 const auth = require('../middleware/auth')
 const User = require('../models/user')
 const multer = require('multer')
+const sharp = require('sharp')
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
@@ -107,7 +108,10 @@ const uploads = multer( {
 } )
 
 router.post('/users/me/avatar', auth, uploads.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer
+    
+    const buffer = await sharp(req.file.buffer).resize({ height: 250, width: 250 }).png().toBuffer()
+
+    req.user.avatar = buffer
     await req.user.save()
     res.send()
 }, (error, req, res, next) => {
@@ -118,6 +122,23 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
     req.user.avatar = undefined
     await req.user.save()
     res.send()
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+
+    try {
+        const user = await User.findById(req.params.id)
+
+        if(!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/png')
+        res.send(user.avatar)
+
+    } catch (e) {
+        res.status(404).send()
+    }
 })
 
 module.exports = router
